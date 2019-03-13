4e8efd60fe2b174c93ea49eec7226cc64ae85f6a
Radu Vancea <Radu.Vancea@msg.group>
2019-03-13 15:30:19 +0200

    9.Olingo - setup

---
+++ src/main/java/ro/msg/learning/servlet/ServletConfiguration.java
@@ -0,0 +1,43 @@
+package ro.msg.learning.servlet;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+public class ServletConfiguration {
+
+    @Bean
+    public ServletRegistrationBean odataServlet(ExampleServlet servlet) {
+        return new ServletRegistrationBean(servlet, "/odata/*");
+    }
+
+    /*@Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
+        LocalContainerEntityManagerFactoryBean emfb = new LocalContainerEntityManagerFactoryBean();
+        emfb.setDataSource(dataSource);
+        emfb.setJpaVendorAdapter(jpaVendorAdapter);
+        emfb.setPersistenceUnitName("h2");
+        emfb.setPackagesToScan("ro.msg.learning.entity");
+        return emfb;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.H2);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(false);
+        adapter.setDatabasePlatform("org.hibernate.dialect.H2Dialect");
+        return adapter;
+    }*/
+
+}
